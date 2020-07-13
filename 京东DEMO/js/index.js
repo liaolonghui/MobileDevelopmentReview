6182d3e30ee4ad91c105ae91487ff18d509feaf1
liaolonghui <1340881930@qq.com>
2020-07-13 21:25:46 +0800

    实现自动轮播

--- 京东DEMO/js/index.js
+++ 京东DEMO/js/index.js
@@ -3,6 +3,8 @@ window.onload = function(){
   searchEffect();
   // 秒杀块的倒计时
   timeBack();
+  // 轮播图
+  bannerEffect();
 }
 
 // 头部搜索块的js效果
@@ -50,4 +52,63 @@ function timeBack(){
     spans[6].innerHTML = Math.floor(second/10);
     spans[7].innerHTML = Math.floor(second%10);
   },1000);
+}
+// 轮播图
+// 1.在首尾添加图片（最前面加一张倒数第一的图，最后面加一张首图。）
+// 2.修改页面结构
+// 3.修改对应样式
+// 4.设置默认偏移(让首图最先显示，而不是刚在最前面添加的倒数第一张图)可以使用 transform: tanslateX(-10%); 或 position结合left:-100%;(应该使用relative定位)
+function bannerEffect(){
+  var banner = document.querySelector('.jd_banner');
+  // 图片容器
+  var imgBox = banner.querySelector('ul:first-of-type');
+  // 1.添加图片
+  var firstImg = imgBox.querySelector('li:first-of-type');
+  var lastImg = imgBox.querySelector('li:last-of-type');
+  imgBox.appendChild(firstImg.cloneNode(true));
+  imgBox.insertBefore(lastImg.cloneNode(true),imgBox.firstChild);
+  // 2.样式
+  var lis = imgBox.querySelectorAll('li');
+  var count = lis.length;
+  var bannerWidth = banner.offsetWidth;
+  // 设置图片盒子的宽度
+  imgBox.style.width = count*bannerWidth + 'px';
+  // 设置每一个li（图片）元素的宽度
+  for(var i=0; i<count; i++){
+    lis[i].style.width = bannerWidth + 'px';
+  }
+  var index = 1;//定义图片索引（从1开始）
+  // 3.设置默认偏移
+  imgBox.style.left = -bannerWidth + 'px';
+  // 4.当屏幕变化时，重新计算宽度
+  window.onresize = function(){
+    // 要改全局的bannerWidth
+    bannerWidth = banner.offsetWidth;
+    // 设置图片盒子的宽度
+    imgBox.style.width = count*bannerWidth + 'px';
+    // 设置每一个li（图片）元素的宽度
+    for(var i=0; i<count; i++){
+      lis[i].style.width = bannerWidth + 'px';
+    }
+    // 偏移（定位值）要考虑到当前索引值
+    imgBox.style.left = -index*bannerWidth + 'px';
+  }
+  // 5.自动轮播(使用定时器)
+  var timer = setInterval(function(){
+    index++;
+    // 添加过渡效果
+    imgBox.style.transition = "left 0.5s ease-in-out";
+    // 偏移
+    imgBox.style.left = (-index*bannerWidth) + 'px';
+    // 要有延迟
+    setTimeout(function(){
+      // 判断是否到最后一张
+      if(index === count-1){
+        index=1;
+        // 偏移到指定位置(别忘了清除过渡效果)
+        imgBox.style.transition = "none";
+        imgBox.style.left = (-index*bannerWidth) + 'px';
+      }
+    },500);
+  },3000);
 }
